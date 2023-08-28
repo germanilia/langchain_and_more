6fe745617cc49b4be5d1223b519258494f81cc57
Ilia <iliag@sela.co.il>
2023-08-28 21:11:41 +0300

    fixed paths

--- examples/summarization.py
+++ examples/summarization.py
@@ -1,10 +1,10 @@
 from services.enums import ModelType,ChainType
 from services.document_analyzer import DocumentAnalyzer
 
-analyzer = DocumentAnalyzer("static/TSLA-Q4-2022-Update.pdf", model_name=ModelType.AZURE)
+analyzer = DocumentAnalyzer("static/tesla/TSLA-Q4-2022-Update.pdf", model_name=ModelType.AZURE)
 analyzer.summarize(ChainType.STUFF)
-# analyzer.summarize(ChainType.REFINE)
-# analyzer.summarize(ChainType.REDUCE)
-# analyzer.stuff_chain()
-# analyzer.map_reduce()
-# analyzer.refine()
+analyzer.summarize(ChainType.REFINE)
+analyzer.summarize(ChainType.MAP_REDUCE)
+analyzer.stuff_chain()
+analyzer.map_reduce()
+analyzer.refine()
